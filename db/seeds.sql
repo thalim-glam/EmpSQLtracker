eaf62be6d1e54c879f6f015a932351f006df7714
Tasneem <thalim.glam@gmail.com>
2023-12-10 23:25:06 -0800

    Inserting data in the tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,8 +13,12 @@ VALUES ("Manager", 70000, 2),
        ("Salesman", 25000, 4);
 
 // Employees Tables
-INSERT INTO employee (title, salary, department_id)
-VALUES ("Manager", 70000, 2),
-       ("Assistant Manager", 50000, 1),
-       ("Accountant", 40000, 3),
-       ("Salesman", 25000, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kamala", "Harris", 1, 1),
+       ("Donald", "Duck", 2, NULL),
+       ("Amalia", "Amman", 3, NULL),
+       ("Victor", "Zaiden", 2, 2),
+       ("Mickey", "Mulan", 3, NULL),
+       ("David", "Copperfield", 2, NULL),
+       ("Jashon", "Joshia", 4, NULL),
+       ("Ratan", "Tataa", 4, NULL);
