4f9a74fe2601f4d85381fd5728a9e520958d0555
Tasneem <thalim.glam@gmail.com>
2023-12-13 11:10:30 -0800

    Editing sql entries for Final version

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,25 +2,27 @@
 INSERT INTO department (name)
 VALUES ("Human Resource"),
        ("Management"),
-       ("Sales"),
-       ("Front Desk");
+       ("Admin"),
+       ("Engineers");
 
 -- Roles Tables
 INSERT INTO role (title, salary, department_id)
-VALUES ("Manager", 70000, 2),
-       ("Assistant Manager", 50000, 2),
-       ("Accountant", 40000, 1),
-       ("Salesman", 25000, 3),
-       ("HR", 30000, 1),
-       ("Intern", 10000, 4);
+VALUES ("Manager", 99000, 2),
+       ("Assistant Manager", 88000, 2),
+       ("Accountant", 35000, 1),
+       ("Receptionist", 21000, 3),
+       ("HR Manager", 45000, 1),
+       ("Product Manager", 65000, 1),
+       ("Techinician", 10000, 4);
 
 -- Employees Tables
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kamala", "Harris", 1, NULL),
+VALUES ("Malala", "Devi", 1, NULL),
        ("Mayson", "Truff", 2, 1),
-       ("Tylor", "Swift", 3, 1),
-       ("Victor", "Zaiden", 2, NULL),
+       ("Tom", "Kash", 3, 1),
+       ("Victor", "Zaiden", 2, 3),
        ("Benjamin", "Huff", 3, 2),
-       ("David", "Copperfield", 2, NULL),
+       ("David", "Chan", 2, NULL),
        ("Weldy", "Josephin", 4, 2),
-       ("Ratan", "Tataa", 4, NULL);
+       ("Malia", "Brown", 4, 3),
+       ("Robert", "Rodrigoz", 2, NULL);
